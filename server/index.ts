acd0b87b32208a4c8688854ce71df711fbd09493
brightside411 <brightsidebirth@gmail.com>
2026-02-28 20:00:46 -0700

    Initial upload of omni-leads content

--- server/index.ts
+++ server/index.ts
@@ -90,11 +90,12 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
+  const port = parseInt(process.env.PORT || process.env.VERCEL_PORT || "5000", 10);
+  const host = process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1";
   httpServer.listen(
     {
       port,
-      host: "127.0.0.1",
+      host,
     },
     () => {
       log(`serving on port ${port}`);
